4eb0a15fea2290584250468bc2a477afcc3fc23e
Moi Lopez <66094710+mlopezuno@users.noreply.github.com>
2023-03-27 19:53:59 -0600

    Add files via upload
    examen_moises

---
+++ Examen/src/Examen.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Examen {
+    public static void main(String[] args) {
+        List alumnos = new ArrayList();//creo una lista
+        alumnos.add("Juan");
+        alumnos.add("Leonardo");
+        alumnos.add("Patricia");
+        alumnos.add("Diego");
+        alumnos.add("Elizabeth");
+        alumnos.add("Maria");
+        alumnos.add("Rosa");
+        alumnos.add("Luis");
+        alumnos.add("Mariel");
+        alumnos.add("Enrique");
+
+        System.out.println(alumnos);//imprimimos lista
+    }
+}
